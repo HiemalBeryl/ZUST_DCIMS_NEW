d28a2427f1485f647422734c4b0661b205c8629d
HiemalBeryl <973650812@qq.com>
2023-04-30 21:42:33 +0800

    2023/04/30 前端-对竞赛信息和竞赛立项的界面进行修改

--- ruoyi-ui/src/views/dcims/competition/index.vue
+++ ruoyi-ui/src/views/dcims/competition/index.vue
@@ -104,27 +104,6 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          v-hasPermi="['dcims:competition:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['dcims:competition:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
           type="danger"
           plain
           icon="el-icon-delete"
@@ -185,13 +164,6 @@
           <el-button
             size="mini"
             type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            v-hasPermi="['dcims:competition:edit']"
-          >修改</el-button>
-          <el-button
-            size="mini"
-            type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['dcims:competition:remove']"
@@ -208,83 +180,12 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改竞赛赛事基本信息对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="赛事名称" prop="name">
-          <el-input v-model="form.name" placeholder="请输入赛事名称" />
-        </el-form-item>
-        <el-form-item label="往届名称" prop="pastName">
-          <el-input v-model="form.pastName" placeholder="请输入往届名称" />
-        </el-form-item>
-        <el-form-item label="赛事官网" prop="website">
-          <el-input v-model="form.website" placeholder="请输入赛事官网" />
-        </el-form-item>
-        <el-form-item label="赛事届次" prop="term">
-          <el-input v-model="form.term" placeholder="请输入赛事届次" />
-        </el-form-item>
-        <el-form-item label="赛事年份" prop="annual">
-          <el-input v-model="form.annual" placeholder="请输入赛事年份" />
-        </el-form-item>
-        <el-form-item label="主办单位" prop="organizer">
-          <el-input v-model="form.organizer" placeholder="请输入主办单位" />
-        </el-form-item>
-        <el-form-item label="竞赛负责人工号" prop="responsiblePersonId">
-          <el-input v-model="form.responsiblePersonId" placeholder="请输入竞赛负责人工号" />
-        </el-form-item>
-        <el-form-item label="竞赛负责人" prop="responsiblePersonName">
-          <el-input v-model="form.responsiblePersonName" placeholder="请输入竞赛负责人" />
-        </el-form-item>
-        <el-form-item label="校内选拔时间" prop="innerTime">
-          <el-date-picker clearable
-            v-model="form.innerTime"
-            type="datetime"
-            value-format="yyyy-MM-dd HH:mm:ss"
-            placeholder="请选择校内选拔时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="省赛时间" prop="provinceTime">
-          <el-date-picker clearable
-            v-model="form.provinceTime"
-            type="datetime"
-            value-format="yyyy-MM-dd HH:mm:ss"
-            placeholder="请选择省赛时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="国赛时间" prop="nationalTime">
-          <el-date-picker clearable
-            v-model="form.nationalTime"
-            type="datetime"
-            value-format="yyyy-MM-dd HH:mm:ss"
-            placeholder="请选择国赛时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="本年度申报经费" prop="budget">
-          <el-input v-model="form.budget" placeholder="请输入本年度申报经费" />
-        </el-form-item>
-        <el-form-item label="获奖目标" prop="goal">
-          <el-input v-model="form.goal" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-        <el-form-item label="赛事简介" prop="introduction">
-          <el-input v-model="form.introduction" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-        <el-form-item label="竞赛申报书" prop="attachment">
-          <file-upload v-model="form.attachment"/>
-        </el-form-item>
-        <el-form-item label="审核人工号" prop="nextAuditId">
-          <el-input v-model="form.nextAuditId" placeholder="请输入审核人工号" />
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
-        <el-button @click="cancel">取 消</el-button>
-      </div>
-    </el-dialog>
+    
   </div>
 </template>
 
 <script>
-import { listCompetition, getCompetition, delCompetition, addCompetition, updateCompetition } from "@/api/dcims/competition";
+import { listCompetition, getCompetition, delCompetition} from "@/api/dcims/competition";
 
 export default {
   name: "Competition",
@@ -336,39 +237,6 @@ export default {
       },
       // 表单参数
       form: {},
-      // 表单校验
-      rules: {
-        id: [
-          { required: true, message: "主键不能为空", trigger: "blur" }
-        ],
-        orderNum: [
-          { required: true, message: "排序号不能为空", trigger: "blur" }
-        ],
-        name: [
-          { required: true, message: "赛事名称不能为空", trigger: "blur" }
-        ],
-        level: [
-          { required: true, message: "赛事类别不能为空", trigger: "change" }
-        ],
-        term: [
-          { required: true, message: "赛事届次不能为空", trigger: "blur" }
-        ],
-        annual: [
-          { required: true, message: "赛事年份不能为空", trigger: "blur" }
-        ],
-        organizer: [
-          { required: true, message: "主办单位不能为空", trigger: "blur" }
-        ],
-        responsiblePersonId: [
-          { required: true, message: "竞赛负责人工号不能为空", trigger: "blur" }
-        ],
-        responsiblePersonName: [
-          { required: true, message: "竞赛负责人不能为空", trigger: "blur" }
-        ],
-        attachment: [
-          { required: true, message: "竞赛申报书不能为空", trigger: "blur" }
-        ],
-      }
     };
   },
   created() {
@@ -406,44 +274,6 @@ export default {
       this.open = false;
       this.reset();
     },
-    // 表单重置
-    reset() {
-      this.form = {
-        id: undefined,
-        orderNum: undefined,
-        name: undefined,
-        level: undefined,
-        pastName: undefined,
-        website: undefined,
-        term: undefined,
-        annual: undefined,
-        organizer: undefined,
-        responsiblePersonId: undefined,
-        responsiblePersonName: undefined,
-        innerTime: undefined,
-        provinceTime: undefined,
-        nationalTime: undefined,
-        stopTime: undefined,
-        budget: undefined,
-        appropriation: undefined,
-        goal: undefined,
-        introduction: undefined,
-        attachment: undefined,
-        moneyAggregate: undefined,
-        workloadAggregate: undefined,
-        personLimit: undefined,
-        teamLimit: undefined,
-        nextAuditId: undefined,
-        state: undefined,
-        version: undefined,
-        createTime: undefined,
-        createBy: undefined,
-        updateTime: undefined,
-        updateBy: undefined,
-        delFlag: undefined
-      };
-      this.resetForm("form");
-    },
     /** 搜索按钮操作 */
     handleQuery() {
       this.queryParams.pageNum = 1;
@@ -463,49 +293,6 @@ export default {
       this.ids = selection.map(item => item.id)
       this.single = selection.length!==1
       this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加竞赛赛事基本信息";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.loading = true;
-      this.reset();
-      const id = row.id || this.ids
-      getCompetition(id).then(response => {
-        this.loading = false;
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改竞赛赛事基本信息";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          this.buttonLoading = true;
-          if (this.form.id != null) {
-            updateCompetition(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            }).finally(() => {
-              this.buttonLoading = false;
-            });
-          } else {
-            addCompetition(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            }).finally(() => {
-              this.buttonLoading = false;
-            });
-          }
-        }
-      });
     },
     /** 删除按钮操作 */
     handleDelete(row) {
--- ruoyi-ui/src/views/dcims/establish/index.vue
+++ ruoyi-ui/src/views/dcims/establish/index.vue
@@ -1,443 +1,193 @@
 <template> <!--赛事立项 -->
 <div>
+  <div style="border-bottom: 1px solid #dbdbdb;height:60px;width:600px ;">
+    <h1 style="margin-top:20px">竞赛立项</h1>
+  </div>
 
-   <div style="border-bottom: 1px solid #dbdbdb;height:60px;width:600px ;">
-     <h1 style="margin-top:20px">竞赛立项</h1>
-   </div>
-   
-  <el-form :model="ruleForm"  ref="ruleForm" label-width="150px" class="demo-ruleForm" label-position="right" style="margin-top:20px">
-  
-
- <el-row>
-   <el-col>
- <el-form-item label="赛事名称" prop="name" required>
-    <el-input type="text" v-model="ruleForm.name" style="width:400px" placeholder="中国大学生服务外包创新创业大赛"></el-input>
-  </el-form-item>
-  </el-col>
-
- <el-col :span="5">
-  <el-form-item label="立项届次" prop="term" >
-    <el-input type="text" v-model="ruleForm.term" style="width:100px" placeholder="2届"></el-input>
-  </el-form-item>
-   </el-col>
-
-
-<el-col :span="5">
-<el-form-item label="赛事性质" style="width:250px">
- <el-select v-model="ruleForm.level" placeholder="A类">
-      
-      <el-option label="A类" value="A类"></el-option>
-       <el-option label="B类" value="B类"></el-option>
-        <el-option label="C类" value="C类"></el-option>
-        <el-option label="其他" value="其他"></el-option>
-
-    </el-select> 
-
-  </el-form-item>
-
-</el-col>
-
-<el-col :span="5">
-   <el-form-item label="赛事年份" prop="annual"  required>
-    <el-input type="text" v-model="ruleForm.annual" style="width:150px" ></el-input>
-  </el-form-item>
-</el-col>
-
-
- </el-row>
-
-
-
-<el-row>
-<el-col :span="5">
-   <el-form-item label="主办单位" prop="organizer"  required>
-    <el-input type="text" v-model="ruleForm.organizer" style="width:150px" ></el-input>
-  </el-form-item>
-</el-col>
-
-<el-col :span="6">
-     <el-form-item label="竞赛负责人" prop="responsiblePersonName"  required>
-    <el-input type="text" v-model="ruleForm.responsiblePersonName" style="width:150px" ></el-input>
-  </el-form-item>
-</el-col>
-
-<el-col :span="5">
-   <el-form-item label="竞赛负责人工号" prop="responsiblePersonId"  required>
-    <el-input type="text" v-model="ruleForm.responsiblePersonId" style="width:150px" ></el-input>
-  </el-form-item>
-</el-col>
-</el-row>
-
-
-
-
-<el-row>
-<el-col :span="5">
-   <el-form-item label="立项开始时间" prop="startTime" >
-    <el-input type="text" v-model="ruleForm.startTime" style="width:150px" ></el-input>
-  </el-form-item>
-</el-col>
-
-<el-col :span="5">
-   <el-form-item label="立项结束时间" prop="stopTime" >
-    <el-input type="text" v-model="ruleForm.stopTime" style="width:150px" ></el-input>
-  </el-form-item>
-</el-col>
-
-</el-row>
-
- 
-  <!-- <el-form-item label="赛事性质" prop="type">
-     <el-radio-group v-model="ruleForm.radio">
-    <el-radio :label="3">A类</el-radio>
-    <el-radio :label="6">B类</el-radio>
-    <el-radio :label="9">C类</el-radio>
-    <el-radio :label="12">其他</el-radio>
-  </el-radio-group>
-  </el-form-item> -->
-
-
-
-  
-
-<!-- <el-form-item label="立项时间" style="width:500px ">
-    <el-col :span="11">
-      <el-form-item prop="day">
-        <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.day" style="width: 100%;"></el-date-picker>
+  <!-- 添加或修改竞赛赛事基本信息对话框 -->
+  <div>
+    <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <el-form-item label="赛事名称" prop="name">
+        <el-input v-model="form.name" placeholder="请输入赛事名称" />
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-form-item prop="time">
-        <el-time-picker placeholder="选择时间" v-model="ruleForm.time" style="width: 100%;"></el-time-picker>
+      <el-form-item label="往届名称" prop="pastName">
+        <el-input v-model="form.pastName" placeholder="请输入往届名称" />
       </el-form-item>
-    </el-col>
-  </el-form-item> -->
-
-
-
-    <el-form-item label="校内选拔赛时间" required style="width:500px">
-    <el-col :span="11">
-      <el-form-item prop="date1">
-        <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.date1" style="width: 100%;"></el-date-picker>
+      <el-form-item label="赛事官网" prop="website">
+        <el-input v-model="form.website" placeholder="请输入赛事官网" />
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-form-item prop="date2">
-        <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
+      <el-form-item label="赛事届次" prop="term">
+        <el-input v-model="form.term" placeholder="请输入赛事届次" />
       </el-form-item>
-    </el-col>
-  </el-form-item>
-
-
-    <el-form-item label="省赛时间" required style="width:500px">
-    <el-col :span="11">
-      <el-form-item prop="prodate1">
-        <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.prodate1" style="width: 100%;"></el-date-picker>
+      <el-form-item label="赛事年份" prop="annual">
+        <el-input v-model="form.annual" placeholder="请输入赛事年份" />
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-form-item prop="prodate2">
-        <el-time-picker placeholder="选择时间" v-model="ruleForm.prodate2" style="width: 100%;"></el-time-picker>
+      <el-form-item label="主办单位" prop="organizer">
+        <el-input v-model="form.organizer" placeholder="请输入主办单位" />
       </el-form-item>
-    </el-col>
-  </el-form-item>
-
-
-    <el-form-item label="国赛时间" required style="width:500px">
-    <el-col :span="11">
-      <el-form-item prop="bigdate1">
-        <el-date-picker type="date" placeholder="选择日期" v-model="ruleForm.bigdate1" style="width: 100%;"></el-date-picker>
+      <el-form-item label="竞赛负责人工号" prop="responsiblePersonId">
+        <el-input v-model="form.responsiblePersonId" placeholder="请输入竞赛负责人工号" />
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">-</el-col>
-    <el-col :span="11">
-      <el-form-item prop="bigdate2">
-        <el-time-picker placeholder="选择时间" v-model="ruleForm.bigdate2" style="width: 100%;"></el-time-picker>
+      <el-form-item label="竞赛负责人" prop="responsiblePersonName">
+        <el-input v-model="form.responsiblePersonName" placeholder="请输入竞赛负责人" />
       </el-form-item>
-    </el-col>
-  </el-form-item>
-
-
-
-<el-row>
-  <el-col :span="6">
-
-   <el-form-item label="本年度申报经费"  required style="width:500px" >
-    <el-col :span="11">
-      <el-form-item prop="yearmoney">
-        <el-input type="number" v-model="ruleForm.yearmoney" ></el-input>
+      <el-form-item label="校内选拔时间" prop="innerTime">
+        <el-date-picker clearable
+          v-model="form.innerTime"
+          type="datetime"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          placeholder="请选择校内选拔时间">
+        </el-date-picker>
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">万元</el-col>
-    </el-form-item>
-
-  </el-col>
-
-  <el-col :span="6">
-
-
-    <el-form-item label="本年度拨款"  required style="width:500px" >
-    <el-col :span="11">
-      <el-form-item prop="yearneedmoney">
-        <el-input type="number" v-model="ruleForm.yearneedmoney" ></el-input>
+      <el-form-item label="省赛时间" prop="provinceTime">
+        <el-date-picker clearable
+          v-model="form.provinceTime"
+          type="datetime"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          placeholder="请选择省赛时间">
+        </el-date-picker>
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">万元</el-col>
-    </el-form-item>
-
-  </el-col>
-  
-  
-  <el-col :span="6">
-
-
-    <el-form-item label="集中授课时数"  required style="width:500px" >
-    <el-col :span="11">
-      <el-form-item prop="teachtime">
-        <el-input type="number" v-model="ruleForm.teachtime" ></el-input>
+      <el-form-item label="国赛时间" prop="nationalTime">
+        <el-date-picker clearable
+          v-model="form.nationalTime"
+          type="datetime"
+          value-format="yyyy-MM-dd HH:mm:ss"
+          placeholder="请选择国赛时间">
+        </el-date-picker>
       </el-form-item>
-    </el-col>
-    <el-col class="line" :span="2">课时</el-col>
-    </el-form-item>
-
-
-  </el-col>
-
-  </el-row>
-  
-
-  
-     
-  <el-form-item label="本年度获奖目标" prop="rewardtarget">
-    <el-input type="text" v-model="ruleForm.rewardtarget" style="width:300px" ></el-input>
-  </el-form-item>
-
-
-
- 
-<!--竞赛申报书上传 -->
-<el-form-item label="竞赛申报书上传:">
-  
-<el-upload
-  class="avatar-uploader"
-  action="https://jsonplaceholder.typicode.com/posts/"
-  :show-file-list="false"
-  :on-success="handleAvatarSuccess"
-  :before-upload="beforeAvatarUpload">
-  <img v-if="imageUrl" :src="imageUrl" class="avatar">
-  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-</el-upload>
-</el-form-item>
-
-
-
-
-  <!-- 最下方按钮 -->
-  <el-form-item>
-    <el-button type="primary" @click="submitForm(ruleForm)">立即创建</el-button>
-    <el-button @click="resetForm('ruleForm')">重置</el-button>
-  </el-form-item>
-</el-form>
-
+      <el-form-item label="本年度申报经费" prop="budget">
+        <el-input v-model="form.budget" placeholder="请输入本年度申报经费" />
+      </el-form-item>
+      <el-form-item label="获奖目标" prop="goal">
+        <el-input v-model="form.goal" type="textarea" placeholder="请输入内容" />
+      </el-form-item>
+      <el-form-item label="赛事简介" prop="introduction">
+        <el-input v-model="form.introduction" type="textarea" placeholder="请输入内容" />
+      </el-form-item>
+      <el-form-item label="竞赛申报书" prop="attachment">
+        <file-upload v-model="form.attachment"/>
+      </el-form-item>
+      <el-form-item label="审核人工号" prop="nextAuditId">
+        <el-input v-model="form.nextAuditId" placeholder="请输入审核人工号" />
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button :loading="buttonLoading" type="primary" @click="submitForm">确 定</el-button>
+      <el-button @click="cancel">取 消</el-button>
+    </div>
+  </div>
 </div>
 </template>
 <script>
-import axios from 'axios'
-  export default {
-      name:'LiXiang',
-    data() {
-      return {
-        //用于上传图片的data
-        imageUrl: '',
-        //
-      
-
-      
-        ruleForm: {
-          level:'',         //赛事性质
-          name: '', //立项赛事名称
-          term:'',   //立项届次
-          annual:'', //赛事年份
-
-          startTime: '', //立项时间
-          stopTime:'', //立项结束时间
-          innerTime:'',     //校内选拔赛时间
-          provinceTime:'',//省赛时间
-          nationalTime:'' , //国赛时间
-          
-          nextAuditId:''  , //审核人id        
-          organizer:'',   //主办单位
-          responsiblePersonName:'', //赛事负责人
-          responsiblePersonId:'',   //赛事负责人ID
-          
-          prodate1:'',      //省赛时间
-          prodate2:'',
-          bigdate1:'',      //国赛时间1
-          bigdate2:'',      //国赛时间
-          yearmoney:'',     //年度申报经费
-          yearneedmoney:'', //年度拨款
-          teachtime:'',     //授课时长
-          rewardtarget:'',  //本年度获奖目标
-          type: '',
-        
-          resource: '',
-          desc: ''
-        },
-         time1:'',
-         person:'',
-         person2:'',
-         money:'',
-         money2:'',
-         teachtime:'',
-         rewardtarget:'',
-
-         
-        rules: {
-          name: [
-            { required: true, message: "请输入活动名称", trigger: 'blur' },
-            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-          ],
-          region: [
-            { required: true, message: '请选择活动区域', trigger: 'change' }
-          ],
-          date1: [
-            { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
-          ],
-          date2: [
-            { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
-          ],
-          type: [
-            { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-          ],
-          resource: [
-            { required: true, message: '请选择活动资源', trigger: 'change' }
-          ],
-          desc: [
-            { required: true, message: '请填写活动形式', trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    methods: {
-
-      //上交项目
-      submitForm(formName){
-
-         var data={
-            "name": formName.name,
-            "level":formName.level,
-            'term':formName.term,
-            "annual": formName.annual,
-            "responsiblePersonName": formName.responsiblePersonName,
-            "organizer":formName.organizer,
-            'responsiblePersonId':formName.responsiblePersonId
-              
-         }
-          
-          const axios1 = axios.create({
-            baseURL:'/dev-api/dcims',
-          }) 
-          // axios.post({
-          //    baseURL:'',
-             
-          // })
-
-
-            var config = {
-              method: 'post',
-               headers: {
-                'Authorization':"Bearer " + window.localStorage.getItem('token')
-                },
-              url: '/competition',
-              data:data
-            };
-
-            axios1(config)
-            .then(function (response) {
-              console.log(JSON.stringify(response.data));
-              alert("立项增加成功 请前往当前年份赛事信息查看")
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-
-
-
-
-
-
-        // this.$refs[formName].validate((valid) => {
-        //   if (valid) {
-        //     alert('submit!');
-        //   } else {
-        //     console.log('error submit!!');
-        //     return false;
-        //   }
-        // });
-      },
-
-
-
-
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      },
-  
-
-//用于上传图片格式限制
-
-           handleAvatarSuccess(res, file) {
-        this.imageUrl = URL.createObjectURL(file.raw);
-      },
-
-
-      
-      beforeAvatarUpload(file) {
-        const isJPG = file.type === 'image/jpeg';
-        const isLt2M = file.size / 1024 / 1024 < 2;
-
-        if (!isJPG) {
-          this.$message.error('上传头像图片只能是 JPG 格式!');
-        }
-        if (!isLt2M) {
-          this.$message.error('上传头像图片大小不能超过 2MB!');
-        }
-        return isJPG && isLt2M;
+import {addCompetition} from "@/api/dcims/competition";
+export default {
+  name: "Competition",
+  dicts: ['dcims_audit_result', 'dcims_competition_type'],
+  data(){
+    return{
+      // 按钮loading
+      buttonLoading: false,
+      // 表单参数
+      form: {},
+      // 是否显示弹出层
+      open: false,
+      // 表单校验
+      rules: {
+        id: [
+          { required: true, message: "主键不能为空", trigger: "blur" }
+        ],
+        orderNum: [
+          { required: true, message: "排序号不能为空", trigger: "blur" }
+        ],
+        name: [
+          { required: true, message: "赛事名称不能为空", trigger: "blur" }
+        ],
+        level: [
+          { required: true, message: "赛事类别不能为空", trigger: "change" }
+        ],
+        term: [
+          { required: true, message: "赛事届次不能为空", trigger: "blur" }
+        ],
+        annual: [
+          { required: true, message: "赛事年份不能为空", trigger: "blur" }
+        ],
+        organizer: [
+          { required: true, message: "主办单位不能为空", trigger: "blur" }
+        ],
+        responsiblePersonId: [
+          { required: true, message: "竞赛负责人工号不能为空", trigger: "blur" }
+        ],
+        responsiblePersonName: [
+          { required: true, message: "竞赛负责人不能为空", trigger: "blur" }
+        ],
+        attachment: [
+          { required: true, message: "竞赛申报书不能为空", trigger: "blur" }
+        ],
       }
     }
+  },
+  created(){
+    
+  },
+  methods: {
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          this.buttonLoading = true;
+          addCompetition(this.form).then(response => {
+            this.$modal.msgSuccess("竞赛立项成功，请等待审核！");
+            this.open = false;
+          }).finally(() => {
+            this.buttonLoading = false;
+            this.reset();
+          });
+        }
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: undefined,
+        orderNum: undefined,
+        name: undefined,
+        level: undefined,
+        pastName: undefined,
+        website: undefined,
+        term: undefined,
+        annual: undefined,
+        organizer: undefined,
+        responsiblePersonId: undefined,
+        responsiblePersonName: undefined,
+        innerTime: undefined,
+        provinceTime: undefined,
+        nationalTime: undefined,
+        stopTime: undefined,
+        budget: undefined,
+        appropriation: undefined,
+        goal: undefined,
+        introduction: undefined,
+        attachment: undefined,
+        moneyAggregate: undefined,
+        workloadAggregate: undefined,
+        personLimit: undefined,
+        teamLimit: undefined,
+        nextAuditId: undefined,
+        state: undefined,
+        version: undefined,
+        createTime: undefined,
+        createBy: undefined,
+        updateTime: undefined,
+        updateBy: undefined,
+        delFlag: undefined
+      };
+      this.resetForm("form");
+    }
   }
+}
 </script>
-
-
-
-<style  scoped>
-   /* 此处style全部为申报书上传style */
-  .avatar-uploader  {
-    width:300px;
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
-  }
-</style>>
-  
