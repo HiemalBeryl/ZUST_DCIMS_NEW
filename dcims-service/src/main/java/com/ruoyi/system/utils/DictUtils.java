7cc1e53f50e3a77e53fa9374535e4c6c78ef47b8
HiemalBeryl <973650812@qq.com>
2023-05-04 16:34:38 +0800

    2023/05/04 后端-将同步竞赛信息功能加入字典工具类

--- dcims-service/src/main/java/com/ruoyi/system/utils/DictUtils.java
+++ dcims-service/src/main/java/com/ruoyi/system/utils/DictUtils.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ruoyi.common.core.domain.entity.SysDictData;
 import com.ruoyi.common.core.domain.entity.SysDictType;
+import com.ruoyi.system.domain.DcimsCompetition;
+import com.ruoyi.system.mapper.DcimsCompetitionMapper;
 import com.ruoyi.system.utils.domain.DcimsStudent;
 import com.ruoyi.system.utils.domain.DcimsTeacher;
 import com.ruoyi.system.utils.mapper.DcimsStudentMapper;
@@ -23,6 +25,7 @@ public class DictUtils {
     private static DictTypeMapper dictTypeMapper;
     private static DcimsStudentMapper dcimsStudentMapper;
     private static DcimsTeacherMapper dcimsTeacherMapper;
+    private static DcimsCompetitionMapper dcimsCompetitionMapper;
 
     @Autowired
     public DictUtils(DictDataMapper dictDataMapper, DictTypeMapper dictTypeMapper, DcimsStudentMapper dcimsStudentMapper, DcimsTeacherMapper dcimsTeacherMapper){
@@ -89,6 +92,44 @@ public class DictUtils {
             allData = allData + '('+Math.toIntExact(student.getStudentId())+','+'\"'+student.getName()+'\"'+','+student.getStudentId()+','+'\"'+"dcims_student"+'\"'+','+'\"'+"default"+'\"'+','+'\''+"N"+'\''+','+"0"+')'+",";
             index++;
             if (index % 5000 == 0){
+                allData = allData.substring(0, allData.length() - 1);
+                allData += ";";
+                flag = dictDataMapper.InsertList(allData);
+                if(flag == false){
+                    dictDataMapper.delete(dictDataQueryWrapper);
+                    return false;
+                }
+                allData = "";
+            }
+        }
+        allData = allData.substring(0, allData.length() - 1);
+        allData += ";";
+        return dictDataMapper.InsertList(allData);
+    }
+
+    /**
+     * 同步年度竞赛字典
+     */
+    public static Boolean syncCompetitionDict(int year){
+        // 查询字典id
+        LambdaQueryWrapper<SysDictType> dictTypeQueryWrapper = new LambdaQueryWrapper<>();
+        dictTypeQueryWrapper.eq(SysDictType::getDictType,"dcims_"+year+"competition");
+        SysDictType sysDictType = dictTypeMapper.selectOne(dictTypeQueryWrapper);
+        // 删除旧字典数据
+        LambdaQueryWrapper<SysDictData> dictDataQueryWrapper = new LambdaQueryWrapper<>();
+        dictDataQueryWrapper.eq(SysDictData::getDictType,"dcims_"+year+"competition");
+        dictDataMapper.delete(dictDataQueryWrapper);
+        // 创建新字典数据
+        String allData = new String();
+        LambdaQueryWrapper<DcimsCompetition> competitionQueryWrapper = new LambdaQueryWrapper<>();
+        competitionQueryWrapper.eq(DcimsCompetition::getAnnual,year);
+        List<DcimsCompetition> competitions = dcimsCompetitionMapper.selectList(competitionQueryWrapper);
+        int index = 0;
+        boolean flag;
+        for(DcimsCompetition competition:competitions){
+            allData = allData + '('+Math.toIntExact(competition.getId())+','+'\"'+competition.getName()+'\"'+','+competition.getId()+','+'\"'+"dcims_"+year+"competition"+'\"'+','+'\"'+"default"+'\"'+','+'\''+"N"+'\''+','+"0"+')'+",";
+            index++;
+            if (index % 50 == 0){
                 allData = allData.substring(0, allData.length() - 1);
                 allData += ";";
                 flag = dictDataMapper.InsertList(allData);
