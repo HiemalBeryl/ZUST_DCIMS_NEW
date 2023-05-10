471e51b365374a669c1a6009eb60bbd9b346e2b6
HiemalBeryl <973650812@qq.com>
2023-05-10 16:33:51 +0800

    2023/05/10 为竞赛基本信息数据库添加了所属学院字段，并修改了对应的前端界面

--- dcims-service/src/main/java/com/ruoyi/system/service/impl/DcimsCompetitionServiceImpl.java
+++ dcims-service/src/main/java/com/ruoyi/system/service/impl/DcimsCompetitionServiceImpl.java
@@ -69,7 +69,7 @@ public class DcimsCompetitionServiceImpl implements IDcimsCompetitionService {
         lqw.eq(bo.getAnnual() != null, DcimsCompetition::getAnnual, bo.getAnnual());
         lqw.eq(StringUtils.isNotBlank(bo.getOrganizer()), DcimsCompetition::getOrganizer, bo.getOrganizer());
         lqw.like(StringUtils.isNotBlank(bo.getResponsiblePersonName()), DcimsCompetition::getResponsiblePersonName, bo.getResponsiblePersonName());
-        lqw.like(StringUtils.isNotBlank(bo.getCollege()), DcimsCompetition::getCollege, bo.getCollege());
+        lqw.like(bo.getCollege() != null, DcimsCompetition::getCollege, bo.getCollege());
         lqw.between(params.get("beginInnerTime") != null && params.get("endInnerTime") != null,
             DcimsCompetition::getInnerTime ,params.get("beginInnerTime"), params.get("endInnerTime"));
         lqw.between(params.get("beginProvinceTime") != null && params.get("endProvinceTime") != null,
