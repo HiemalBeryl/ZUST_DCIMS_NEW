26553586ef7df2543afe81b89c95b2226995c23f
HiemalBeryl <973650812@qq.com>
2024-09-22 18:15:38 +0800

    审查代码，修改了部分地方

--- dcims-service/src/main/java/com/ruoyi/system/service/IDcimsTeamService.java
+++ dcims-service/src/main/java/com/ruoyi/system/service/IDcimsTeamService.java
@@ -47,10 +47,6 @@ public interface IDcimsTeamService {
      */
     List<DcimsTeamVo> queryList(DcimsTeamBo bo);
 
-    /**
-     * 导出Excel
-     */
-    List<DcimsTeamExportExcel> exportExcel(DcimsTeamBo bo);
 
     /**
      * 新增参赛团队
